74f0c0612e69f235729620503a4739e75430683a
David Lee <ambitiouspear@gmail.com>
2015-01-04 23:08:19 -0800

    Add settings controller (default tip, color theme), currency format, and guest split

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -10,29 +10,19 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var defaultColorThemeButton: UISegmentedControl!
     @IBOutlet weak var defaultTipButton: UISegmentedControl!
+    @IBOutlet weak var defaultColorThemeButton: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        NSUserDefaults.resetStandardUserDefaults()
         var defaults = NSUserDefaults.standardUserDefaults()
-        if(defaults.objectForKey("defaultTip") != nil) {
-          var defaultTipValue = defaults.integerForKey("defaultTip")
-        } else {
-           defaults.setInteger(1, forKey: "defaultTip")
-           var defaultTipValue = 1
-        }
-        println(defaults.objectForKey("defaultTip"))
-        
-        if(defaults.objectForKey("colorTheme") != nil) {
-            var colorThemeValue = defaults.integerForKey("colorTheme")
-        } else {
-            defaults.setInteger(0, forKey: "colorTheme")
-            var colorThemeValue = 1
-        }
-        defaults.synchronize()
 
-        // Do any additional setup after loading the view.
+        let defaultTip = defaults.integerForKey("defaultTip")
+        setColorTheme()
+//        println(defaults.integerForKey("defaultTip"))
+        defaultTipButton.selectedSegmentIndex = defaults.integerForKey("defaultTip")
+        defaultColorThemeButton.selectedSegmentIndex = defaults.integerForKey("defaultColor")
+//        let defaultColor = defaults.objectForKey("defaultColor") ?? "light"
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,10 +32,28 @@ class SettingsViewController: UIViewController {
 
     @IBAction func onDefaultTipChanged(sender: AnyObject) {
        NSUserDefaults.standardUserDefaults().setInteger(sender.selectedSegmentIndex, forKey: "defaultTip")
+//        println(NSUserDefaults.standardUserDefaults().integerForKey("defaultTip"))
     }
     
     @IBAction func onColorThemeChanged(sender: AnyObject) {
-       NSUserDefaults.standardUserDefaults().setInteger(sender.selectedSegmentIndex, forKey: "colorTheme")
+       NSUserDefaults.standardUserDefaults().setInteger(sender.selectedSegmentIndex, forKey: "defaultColor")
+        println(NSUserDefaults.standardUserDefaults().integerForKey("defaultColor"))
+        setColorTheme()
+    }
+    
+    func setColorTheme() {
+        if NSUserDefaults.standardUserDefaults().integerForKey("defaultColor") == 1 {
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
+            self.view.backgroundColor = UIColor(red: (34/255.0), green: (139/255.0), blue: (34/255.0), alpha: 1.0)
+            self.view.tintColor = UIColor.whiteColor()
+                }, nil)
+
+        } else {
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
+                self.view.backgroundColor = .whiteColor()
+                self.view.tintColor = UIColor.grayColor()
+            }, nil)
+        }
     }
 
 }
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalForTwo: UILabel!
+    @IBOutlet weak var totalForThree: UILabel!
+    @IBOutlet weak var totalForFour: UILabel!
+    @IBOutlet weak var totalForFive: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,26 @@ class ViewController: UIViewController {
         
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        totalForTwo.text = "$0.00"
+        totalForThree.text = "$0.00"
+        totalForFour.text = "$0.00"
+        totalForFive.text = "$0.00"
+        var defaults = NSUserDefaults.standardUserDefaults()
+        setColorTheme()
+        
+//        if(defaults.integerForKey("defaultTip") == 0) {
+//            tipControl.selectedSegmentIndex = 0
+//            println("no default set")
+////                defaults.integerForKey("defaultTip") ?? 0
+//        } else {
+//            println("default set")
+            tipControl.selectedSegmentIndex = defaults.integerForKey("defaultTip")
+//        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        setColorTheme()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,19 +58,35 @@ class ViewController: UIViewController {
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
-        
-        
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
         
-//        tip = tip.StringByReplacingOccurrencesOfString
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format:"$%.2f", total)
+        tipLabel.text = formatter.stringFromNumber(tip)
+//        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = formatter.stringFromNumber(total)
+        totalForTwo.text = formatter.stringFromNumber(total/2)
+        totalForThree.text = formatter.stringFromNumber(total/3)
+        totalForFour.text = formatter.stringFromNumber(total/4)
+        totalForFive.text = formatter.stringFromNumber(total/5)
+//        totalLabel.text = String(format:"$%.2f", total)
         
     }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
+    }
+    
+    func setColorTheme() {
+        if NSUserDefaults.standardUserDefaults().integerForKey("defaultColor") == 1 {
+            self.view.backgroundColor = UIColor(red: (34/255.0), green: (139/255.0), blue: (34/255.0), alpha: 1.0)
+            self.view.tintColor = UIColor.whiteColor()
+        } else {
+            println("hi")
+            self.view.backgroundColor = .whiteColor()
+            self.view.tintColor = UIColor.grayColor()
+        }
     }
    
 }
